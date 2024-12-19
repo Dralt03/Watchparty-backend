9360794645673eaa33cdafb47efd8f14043e9b36
sauren-sharma <sauren.sharma@zenstreet.ai>
2024-12-19 18:02:36 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const app = require("express")();
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
